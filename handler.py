e1ecd3c3b93cec74f6ad3753d032582cb13db14b
Abhishek Fulzele <be.bune13@gmail.com>
2020-04-13 21:06:06 -0400

    Scanning done

--- handler.py
+++ handler.py
@@ -15,16 +15,22 @@ class DecimalEncoder(json.JSONEncoder):
 
 def main(event, context):
     dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
-
     table = dynamodb.Table('covid-global')
+    result = table.scan()
 
-    response = table.scan()
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(result['Items'], cls=DecimalEncoder)
+    }
 
-    for i in response['Items']:
-        print(json.dumps(i, cls=DecimalEncoder))
+    while 'LastEvaluatedKey' in result:
+        result = table.scan(ExclusiveStartKey=result['LastEvaluatedKey'])
+        response = {
+            "statusCode": 200,
+            "body": json.dumps(result['Items'], cls=DecimalEncoder)
+        }
 
-    while 'LastEvaluatedKey' in response:
-        response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
+    return response
 
-        for i in response['Items']:
-            print(json.dumps(i, cls=DecimalEncoder))
+if __name__ == "__main__":
+    main('', '')
